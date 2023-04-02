c013cdfd6e1691ef13f5f1ea7bf123866663030b
lukashorky <bofhik42@seznam.cz>
2023-04-02 12:00:51 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <WireData.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <LowPower.h>
+
+// I2C slave address
+const int i2cSlaveAddress = 0x08;
+
+// bucket tips counter
+volatile long tipsCount = 0;
+
+// interrupt routine to detect rain gauge tips
+void bucketTipIsr() {
+  tipsCount++;
+  
+}
+
+// i2c routine to report tips counter status
+void i2cRequestIsr() {
+  wireWriteData(tipsCount);
+  Serial.print("Telemetry request.  ");
+  Serial.print(tipsCount);
+  Serial.println(" reported to  Master.");
+
+}
+
+void setup() {
+
+  clock_prescale_set(clock_div_2); // reduce clock speed to 8 MHz
+
+  // set input pin modes
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+
+  // init i2c as slave unit
+  Wire.begin(i2cSlaveAddress);
+  Wire.setClock(10000L);
+  Wire.onRequest(i2cRequestIsr);
+
+  // enable external interrupts to count bucket tips
+  attachInterrupt(digitalPinToInterrupt(2), bucketTipIsr, FALLING);
+  attachInterrupt(digitalPinToInterrupt(3), bucketTipIsr, FALLING);
+
+  // initialize serial line
+  Serial.begin(115200);
+  Serial.println("Wake up, Neo!");
+}
+
+void loop() {
+ 
+ Serial.println("Spink!");
+ delay(200);
+  Serial.flush();
+ // enter sleep monde
+ LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+}
